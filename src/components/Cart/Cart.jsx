ee6a5be2d64251770d4ef0da7c491beb8253a4b2
patildheeraj <dheerajpatil015@gmail.com>
2021-12-31 10:56:27 +0530

    first commit

---
+++ src/components/Cart/Cart.jsx
@@ -0,0 +1,81 @@
+import React from "react";
+import { Container, Typography, Button, Grid } from "@material-ui/core";
+import useStyles from "./styles";
+import CartItem from "./CartItem/CartItem";
+import { Link } from "react-router-dom";
+
+const Cart = ({
+  cart,
+  handleEmptyCart,
+  handleUpdateCartQty,
+  handleRemoveFromCart,
+}) => {
+  const classes = useStyles();
+
+  const EmptyCart = () => (
+    <>
+      <Typography variant="subtitle1">
+        You have no items in your shopping cart,
+        <Link to="/" className={classes.Link}>
+          start adding some items!
+        </Link>
+      </Typography>
+    </>
+  );
+
+  const FilledCart = () => (
+    <>
+      <Grid container spacing={4}>
+        {cart.line_items.map((item) => (
+          <Grid item xs={12} sm={4} key={item.id}>
+            <CartItem
+              item={item}
+              handleUpdateCartQty={handleUpdateCartQty}
+              handleRemoveFromCart={handleRemoveFromCart}
+            />
+          </Grid>
+        ))}
+      </Grid>
+      <div className={classes.cardDetails}>
+        <Typography variant="h4">
+          Subtotal: {cart.subtotal.formatted_with_symbol}
+        </Typography>
+        <div>
+          <Button
+            className={classes.emptyButton}
+            size="large"
+            type="button"
+            variant="contained"
+            color="secondary"
+            onClick={() => handleEmptyCart()}
+          >
+            Empty Cart
+          </Button>
+          <Button
+            className={classes.checkoutButton}
+            component={Link}
+            to="/checkout"
+            size="large"
+            type="button"
+            variant="contained"
+            color="primary"
+          >
+            Checkout
+          </Button>
+        </div>
+      </div>
+    </>
+  );
+
+  return (
+    <Container>
+      <div className={classes.toolbar} />
+      <Typography variant="h3" className={classes.title} gutterBottom>
+        Your Shopping Cart
+      </Typography>
+      {!cart.line_items ? <EmptyCart /> : <FilledCart />}
+    </Container>
+  );
+};
+
+export default Cart;
---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,57 @@
+import React from "react";
+import {
+  AppBar,
+  Toolbar,
+  IconButton,
+  Badge,
+  Typography,
+} from "@material-ui/core";
+import { ShoppingCart } from "@material-ui/icons";
+import logo from "../../assets/commerce.png";
+import useStyles from "./style";
+import { Link, useLocation } from "react-router-dom";
+
+const Navbar = ({ totalItem }) => {
+  const classes = useStyles();
+  const location = useLocation();
+  return (
+    <>
+      <AppBar position="fixed" className={classes.appBar} color="inherit">
+        <Toolbar>
+          <Typography
+            component={Link}
+            to="/"
+            variant="h6"
+            className={classes.title}
+            color="inherit"
+          >
+            <img
+              src={logo}
+              alt="Commerce.js"
+              height="25px"
+              className={classes.image}
+            />
+            E-Shopper
+          </Typography>
+          <div className={classes.grow} />
+          {location.pathname === "/" && (
+            <div className={classes.button}>
+              <IconButton
+                component={Link}
+                to="/cart"
+                aria-label="Show cart items"
+                color="inherit"
+              >
+                <Badge badgeContent={totalItem} color="secondary">
+                  <ShoppingCart />
+                </Badge>
+              </IconButton>
+            </div>
+          )}
+        </Toolbar>
+      </AppBar>
+    </>
+  );
+};
+
+export default Navbar;
